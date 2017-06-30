e688dcc450c9a5b5b84855875b2a82725d5b46eb
GeorgiLalov <lalov.georgi.l@gmail.com>
2017-06-30 23:18:50 +0300

    Adding part of Add and Destroy of ArrayList

--- ListManager/array_list.c
+++ ListManager/array_list.c
@@ -4,8 +4,13 @@
 
 //! Used for malloc.
 #include <stdlib.h>
+//! Used for memmove
+#include <string.h>
 
 #include "array_list.h"
+
+//! The number of bits in one byte.
+#define NUMBER_OF_BITS_IN_BYTE    8
 
 
 /**
@@ -70,7 +75,24 @@ int ArrayList_Add(tsArrayListInfo *fp_pListInfo,
     }
     else
     {
+        //! If array list is full.
+        if (fp_pListInfo->count == fp_pListInfo->totalCapacity)
+        {
 
+        }
+        //! If list is not full, add normally.
+        else
+        {
+            //! Copy the data to the list.
+            //!              Cast to char to know that compiler thinks pHead for 1 byte.
+            //!              dataSize is bit size, so divide by 8 to get how many bytes one node is and multiply by
+            //!              the current count to get to the position of the free memory.
+            memmove((void *) (((char *) fp_pListInfo->pHead) + (fp_pListInfo->count * (fp_pListInfo->dataSize / NUMBER_OF_BITS_IN_BYTE))),
+                    fp_pNewData, fp_pListInfo->dataSize);
+
+            //! Increase the count.
+            fp_pListInfo->count++;
+        }
     }
 
     return retVal;
@@ -82,7 +104,25 @@ int   ArrayList_RemoveAt(tsArrayListInfo    *fp_pListInfo,
                          const unsigned int fp_index);
 void* ArrayList_Get(const tsArrayListInfo *fp_pListInfo,
                     const unsigned int    fp_index);
-void  ArrayList_Destroy(tsArrayListInfo *fp_pListInfo);
+
+/**
+ * @brief Destroy the array list.
+ * @param fp_pListInfo    Pointer to the list info.
+ */
+void ArrayList_Destroy(tsArrayListInfo *fp_pListInfo)
+{
+    //! Check parameter for null.
+    if (fp_pListInfo != NULL)
+    {
+        //! Free the allocated space.
+        free(fp_pListInfo->pHead);
+
+        //! Reset the head and the count of nodes.
+        fp_pListInfo->pHead = NULL;
+        fp_pListInfo->count = 0;
+    }
+}
+
 int   ArrayList_Sort(tsArrayListInfo      *fp_pListInfo,
                      const tCmpNodesFuncP *fp_pCmpFuncP);
 void* ArrayList_Search(const tsArrayListInfo *fp_pListInfo,
