cd8e36f07466a6146f73d5f0dc8cba9fb986a9c4
GeorgiLalov <lalov.georgi.l@gmail.com>
2018-03-02 17:01:25 +0200

    Added implementation of ArrayList_Sort()

--- ListManager/array_list.c
+++ ListManager/array_list.c
@@ -295,11 +295,105 @@ void ArrayList_Destroy(tsArrayListInfo *fp_pListInfo)
     }
 }
 
-int   ArrayList_Sort(tsArrayListInfo      *fp_pListInfo,
-                     const tCmpNodesFuncP *fp_pCmpFuncP);
+/**
+ * @brief Sort the whole list.
+ * @param fp_pListInfo    From which list.
+ * @param fp_pCmpFuncP    
+ * @return
+ *          #0  - On success.
+ *          #-1 - On null parameter.
+ *          #-2 - On error allocating memory.
+ *          #-3 - ArrayList_Get return error.
+ */
+int ArrayList_Sort(tsArrayListInfo         *fp_pListInfo,
+                   const tCmpNodesFuncP    fp_pCmpFuncP,
+                   const teCmpSortingOrder eSortingOrder)
+{
+    int retVal = 0;
+    int endIndex = 0;
+    int cmpIndex = 0;
+    teCmpFuncRetType eCmpRetVal = eCmpFuncRet_Equal;
+    void *pCurrentElement = NULL;
+    void *pNextElement = NULL;
+    
+    if (fp_pListInfo == NULL || fp_pCmpFuncP == NULL)
+    {
+        retVal = -1;
+    }
+    else
+    {
+        //! Iterate through the whole list except the last element and slap.
+        for (endIndex = (fp_pListInfo->count - 1); endIndex > 0 ; endIndex--)
+        {
+            for (cmpIndex = 0; cmpIndex < endIndex; cmpIndex++)
+            {
+                //! Get the current element form the list.
+                pCurrentElement = ArrayList_Get(fp_pListInfo, cmpIndex);
+                //! Get the next element form the list.
+                pNextElement    = ArrayList_Get(fp_pListInfo, cmpIndex + 1);
+                
+                //! On error getting the elements from the list.
+                if (pCurrentElement == NULL || pNextElement == NULL)
+                {
+                    retVal = -3;
+                    
+                    break;
+                }
+                
+                //! Compare two elements next to each other.
+                eCmpRetVal = fp_pCmpFuncP(pCurrentElement,
+                                          pNextElement);
+                                          
+                switch (eSortingOrder)
+                {
+                    //! 
+                    case eAscendingOrder:
+                        //! If the left element is bigger than the right one, slap is needed.
+                        if (eCmpRetVal == eCmpFuncRet_Bigger)
+                        {
+                            retVal = fs_swapTwoNodes(fp_pListInfo,
+                                                     cmpIndex,
+                                                     cmpIndex + 1);
+                        }
+                        
+                        break;
+                        
+                    //! 
+                    case eDescendingOrder:
+                        //! If the left element is smaller than the right one, slap is needed.
+                        if (eCmpRetVal == eCmpFuncRet_Smaller)
+                        {
+                            retVal = fs_swapTwoNodes(fp_pListInfo,
+                                                     cmpIndex,
+                                                     cmpIndex + 1);
+                        }
+                        
+                        break;
+                        
+                }
+                
+                //! On error with slapping the nodes.
+                if (retVal != 0)
+                {
+                    break;
+                }
+            }
+            
+            //! On error.
+            if (retVal != 0)
+            {
+                break;
+            }
+        }
+    }
+    
+    return retVal;
+}
+
+
 void* ArrayList_Search(const tsArrayListInfo *fp_pListInfo,
-                       const tCmpNodesFuncP  *fp_pCmpFuncP,
+                       const tCmpNodesFuncP  fp_pCmpFuncP,
                        const void            *fp_pSearchedData);
 int   ArrayList_SortedInsert(tsArrayListInfo      *fp_pListInfo,
-                             const tCmpNodesFuncP *fp_pCmpFuncP,
+                             const tCmpNodesFuncP fp_pCmpFuncP,
                              const void           *fp_pNewData);
